cfd521aa79fcd09eb645854b0538cd5079148e56
Whats-up-pro <23521308@gm.uit.edu.vn>
2026-01-24 09:34:39 +0700

    revision

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-21a80088'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.ok3hnjbo61"
+    "revision": "0.1hh6cbpr5po"
   }], {
     "ignoreURLParametersMatching": [/^fbclid$/, /^gclid$/, /^utm_/]
   });
